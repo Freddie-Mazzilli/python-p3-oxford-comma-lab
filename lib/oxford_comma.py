ca315da04c9aecc9b95e8144a2913e71463218f4
Freddie Mazzilli <freddie.mazzilli@gmail.com>
2023-05-25 18:35:03 -0400

    Completes oxford comma lab

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,13 @@
+
 def oxford_comma(items):
-    return None
+    if len(items) == 1:
+        items2 = ''.join(items)
+        return items2
+    if len(items) == 2:
+        items2 = ' and '.join(items)
+        return items2
+    else:
+        a = ', '.join(items[:-1])
+        b = ', and ' + items[-1]
+        items2 = a + b
+        return items2
